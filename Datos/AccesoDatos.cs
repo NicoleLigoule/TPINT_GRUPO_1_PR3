b1b9cb528b26406e587802046033f7aad113704f
Dante_dev <109294689+Dantlu@users.noreply.github.com>
2024-06-19 11:40:47 -0300

    Datos: Acceso a datos
    Inicié la conexión con la base de datos y las funciones para cargar los dropdownlists

---
+++ Datos/AccesoDatos.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Datos
+{
+    public class AccesoDatos
+    {
+        private string rutaBDClinica;
+
+        public AccesoDatos()
+        {
+            rutaBDClinica = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=BDClinica;Integrated Security=True";
+        }
+
+        public SqlConnection ObtenerConexion()
+        {
+            SqlConnection cn = new SqlConnection(rutaBDClinica);
+            try
+            {
+                cn.Open();
+                return cn;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public SqlDataReader obtenerReadDDLEspecialidad()
+        {
+            try
+            {
+                string consulta = "SELECT Nombre_esp,ID_esp FROM dbo.Especialidad";
+                AccesoDatos acceso = new AccesoDatos();
+                SqlConnection conexion = acceso.ObtenerConexion();
+
+                SqlCommand commandEsp = new SqlCommand(consulta, conexion);
+
+                SqlDataReader reader = commandEsp.ExecuteReader();
+                return reader;
+            }
+            catch (Exception e)
+            {
+
+                return null;
+            }
+        }
+
+        public SqlDataReader obtenerReadDDLProvincia()
+        {
+            try
+            {
+                string consulta = "SELECT Nombre_prov,ID_prov FROM dbo.Provincia";
+                AccesoDatos acceso = new AccesoDatos();
+                SqlConnection conexion = acceso.ObtenerConexion();
+
+                SqlCommand commandprov = new SqlCommand(consulta, conexion);
+
+                SqlDataReader reader = commandprov.ExecuteReader();
+                return reader;
+            }
+            catch (Exception e)
+            {
+
+                return null;
+            }
+        }
+
+        public SqlDataReader obtenerReadDDLLocalidad()
+        {
+            try
+            {
+                string consulta = "SELECT Nombre_loca,ID_loca FROM dbo.Localidad";
+                AccesoDatos acceso = new AccesoDatos();
+                SqlConnection conexion = acceso.ObtenerConexion();
+
+                SqlCommand commandLoca = new SqlCommand(consulta, conexion);
+
+                SqlDataReader reader = commandLoca.ExecuteReader();
+                return reader;
+            }
+            catch (Exception e)
+            {
+
+                return null;
+            }
+        }
+    }
+}
