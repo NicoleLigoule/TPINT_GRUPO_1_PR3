98f2bde4620a428d6c689bc26de4ce304d05ea82
AceitunaGuerrera <tomasmunguia300821@hotmail.com>
2024-06-19 12:38:19 -0300

    Datos: Datoprovincia y Entidades: Provincia

---
+++ Datos/DatoProvincia.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Entidades;
+namespace Datos
+{
+    public class DatoProvincia
+    {
+        public List<Provincia> ObtenerProvincias()
+        {
+            List<Provincia> provincias = new List<Provincia>();
+            string consulta = "SELECT Id_Provincia, DescripcionProvincia FROM dbo.Provincia";
+
+            AccesoDatos acceso = new AccesoDatos();
+            SqlConnection conexion = acceso.ObtenerConexion();
+
+            SqlCommand commandprov = new SqlCommand(consulta, conexion);
+
+            SqlDataReader reader = commandprov.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    int id_provincia = reader.GetInt32(0);
+                    string DescripcionProvincia = reader.GetString(1);
+                    Provincia prov = new Provincia(id_provincia, DescripcionProvincia);
+
+                    if (!provincias.Contains(prov))
+                    {
+                        provincias.Add(prov);
+
+                    }
+                }
+            }
+            return provincias;
+        }
+    }
+}
---
+++ Entidades/Provincia.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Provincia
+    {
+        private int id_provincia;
+        private string descripcion_Provincia;
+
+        public Provincia(int id, string descripcion)
+        {
+            id_provincia = id;
+            descripcion_Provincia = descripcion;
+        }
+
+        public int GetIdProvincia()
+        {
+            return id_provincia;
+        }
+
+        public void SetIdProvincia(int id)
+        {
+            id_provincia = id;
+        }
+
+        public string GetDescripcionProvincia()
+        {
+            return descripcion_Provincia;
+        }
+
+        public void SetDescripcionProvincia(string descripcion)
+        {
+            descripcion_Provincia = descripcion;
+        }
+    }
+}
