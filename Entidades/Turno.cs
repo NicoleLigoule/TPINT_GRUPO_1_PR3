44ac3f4e5334d483a4a12e1d6ce43301732dd600
danimrios <danimrios09@gmail.com>
2024-06-19 23:09:50 -0300

    negocio: lo anterior
    perdón no le di al botón guardar todo tengo sueño

--- Entidades/Turno.cs
+++ Entidades/Turno.cs
@@ -8,5 +8,57 @@ namespace Entidades
 {
     class Turno
     {
+        int _Legajo;
+        /// hace falta hacer una clase fecha  Fecha
+        int _Horario;
+        string _Asistencia;
+        string _Descripcion;
+
+        public Turno(string Legajo, string Horario, string Asistencia, string Descripcion)
+        {
+            ///falta agregar la clase fecha para poder terminar esto!!!
+            _Legajo= int.Parse(Legajo);
+            _Horario= int.Parse(Horario);
+            _Asistencia = Asistencia;
+            _Descripcion = Descripcion;
+        }
+        public int GetLegajo()
+        {
+            return _Legajo;
+        }
+
+        public void SetLegajo(int Legajo)
+        {
+            _Legajo = Legajo;
+        }
+        public int GetHorario()
+        {
+            return _Horario;
+        }
+
+        public void SetHorario(int Horario)
+        {
+            _Horario = Horario;
+        }
+        public string GetAsistencia()
+        {
+            return _Asistencia;
+        }
+
+        public void SetAsistencia(string Asistencia)
+        {
+            _Asistencia = Asistencia;
+        }
+
+        public string GetDescripcion()
+        {
+            return _Descripcion;
+        }
+
+        public void SetDescripcion(string Descripcion)
+        {
+            _Descripcion = Descripcion;
+        }
+
     }
 }
