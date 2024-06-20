2d53ee6350c625e94e157d0cad547dcd50c66ff6
Dante_dev <109294689+Dantlu@users.noreply.github.com>
2024-06-20 15:44:19 -0300

    entidades: pacientes y medicos
    creé sus clases

---
+++ Entidades/Medico.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Medico
+    {
+        private int Legajo_me;
+        private string DNI_me;
+        private string Nombre_me;
+        private string Apellido_me;
+        private int Localidad_me;
+        private int Provincia_me;
+        private int Especialidad_me;
+        private string Sexo_me;
+        private string Nacionalidad_me;
+        private string FechaNacimiento_me;
+        private string Direccion_me;
+        private string CorreoElectronico_me;
+        private string Telefono_me;
+
+
+        public Medico()
+        {
+
+        }
+
+        public Medico(int legajo_me, string dNI_me, string nombre_me, string apellido_me, int localidad_me, int provincia_me, int especialidad_me, string sexo_me, string nacionalidad_me, string fechaNacimiento_me, string direccion_me, string correoElectronico_me, string telefono_me)
+        {
+            Legajo_me = legajo_me;
+            DNI_me = dNI_me;
+            Nombre_me = nombre_me;
+            Apellido_me = apellido_me;
+            Localidad_me = localidad_me;
+            Provincia_me = provincia_me;
+            Especialidad_me = especialidad_me;
+            Sexo_me = sexo_me;
+            Nacionalidad_me = nacionalidad_me;
+            FechaNacimiento_me = fechaNacimiento_me;
+            Direccion_me = direccion_me;
+            CorreoElectronico_me = correoElectronico_me;
+            Telefono_me = telefono_me;
+        }
+
+        public int Legajo_me1 { get => Legajo_me; set => Legajo_me = value; }
+        public string DNI_me1 { get => DNI_me; set => DNI_me = value; }
+        public string Nombre_me1 { get => Nombre_me; set => Nombre_me = value; }
+        public string Apellido_me1 { get => Apellido_me; set => Apellido_me = value; }
+        public int Localidad_me1 { get => Localidad_me; set => Localidad_me = value; }
+        public int Provincia_me1 { get => Provincia_me; set => Provincia_me = value; }
+        public int Especialidad_me1 { get => Especialidad_me; set => Especialidad_me = value; }
+        public string Sexo_me1 { get => Sexo_me; set => Sexo_me = value; }
+        public string Nacionalidad_me1 { get => Nacionalidad_me; set => Nacionalidad_me = value; }
+        public string FechaNacimiento_me1 { get => FechaNacimiento_me; set => FechaNacimiento_me = value; }
+        public string Direccion_me1 { get => Direccion_me; set => Direccion_me = value; }
+        public string CorreoElectronico_me1 { get => CorreoElectronico_me; set => CorreoElectronico_me = value; }
+        public string Telefono_me1 { get => Telefono_me; set => Telefono_me = value; }
+    }
+}
---
+++ Entidades/Paciente.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Paciente
+    {
+        private string DNI_pc;
+        private string Nombre_pc;
+        private string Apellido_pc;
+        private int Localidad_pc;
+        private int Provincia_pc;
+        private string Sexo_pc;
+        private string Nacionalidad_pc;
+        private string FechaNacimiento_pc;
+        private string Direccion_pc;
+        private string CorreoElectronico_pc;
+        private string Telefono_pc;
+
+        
+
+        public Paciente()
+        {
+
+        }
+
+        public Paciente(string dNI_pc, string nombre_pc, string apellido_pc, int localidad_pc, int provincia_pc, string sexo_pc, string nacionalidad_pc, string fechaNacimiento_pc, string direccion_pc, string correoElectronico_pc, string telefono_pc)
+        {
+            DNI_pc = dNI_pc;
+            Nombre_pc = nombre_pc;
+            Apellido_pc = apellido_pc;
+            Localidad_pc = localidad_pc;
+            Provincia_pc = provincia_pc;
+            Sexo_pc = sexo_pc;
+            Nacionalidad_pc = nacionalidad_pc;
+            FechaNacimiento_pc = fechaNacimiento_pc;
+            Direccion_pc = direccion_pc;
+            CorreoElectronico_pc = correoElectronico_pc;
+            Telefono_pc = telefono_pc;
+        }
+
+        public void setDni(string dni)
+        {
+            this.DNI_pc = dni;
+        }
+
+        public string getDNI()
+        {
+            return DNI_pc;
+        }
+
+        public void setNombre(string nombrePac)
+        {
+            this.Nombre_pc = nombrePac;
+        }
+
+        public string getNombrePac()
+        {
+            return Nombre_pc;
+        }
+
+        public void setApe(string apelllidoPac)
+        {
+            this.Apellido_pc = apelllidoPac;
+        }
+
+        public string getApellido()
+        {
+            return Apellido_pc;
+        }
+
+        public void setLocalidad(int loca)
+        {
+            this.Localidad_pc = loca;
+        }
+
+        public int getLoca()
+        {
+            return Localidad_pc;
+        }
+
+        public void setProv(int prov)
+        {
+            this.Provincia_pc = prov;
+        }
+
+        public int getProv()
+        {
+            return Provincia_pc;
+        }
+
+        public string Sexo_pc1 { get => Sexo_pc; set => Sexo_pc = value; }
+        public string Nacionalidad_pc1 { get => Nacionalidad_pc; set => Nacionalidad_pc = value; }
+        public string FechaNacimiento_pc1 { get => FechaNacimiento_pc; set => FechaNacimiento_pc = value; }
+        public string Direccion_pc1 { get => Direccion_pc; set => Direccion_pc = value; }
+        public string CorreoElectronico_pc1 { get => CorreoElectronico_pc; set => CorreoElectronico_pc = value; }
+        public string Telefono_pc1 { get => Telefono_pc; set => Telefono_pc = value; }
+    }
+}
