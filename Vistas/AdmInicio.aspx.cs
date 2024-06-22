f5e14e8b58eddea63f1da29b4f048d2efab57c6d
NicoleLigoule <nicole.ligoule@alumnos.frgp.utn.edu.ar>
2024-06-22 14:40:19 -0300

    Vistas: AdmInicio.aspx.cs
    Comento la validacion para que nos permita acceder hasta que desarrollemos correctamente la parte de usuario admin

--- Vistas/AdmInicio.aspx.cs
+++ Vistas/AdmInicio.aspx.cs
@@ -11,12 +11,12 @@ namespace Vistas
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            // TO-DO: Necesitamos verificar si el usuario es administrador antes de mostrarle esta página ya que sino puede acceder desde cualquier lado
-            if (!User.IsInRole("Administrador"))
-            {
-                // TO-DO: Si no es admin, redireccionarlo a otra página que sea un aviso.
-                Response.Redirect("PaginaNoAutorizada.aspx");
-            }
+            //// TO-DO: Necesitamos verificar si el usuario es administrador antes de mostrarle esta página ya que sino puede acceder desde cualquier lado
+            //if (!User.IsInRole("Administrador"))
+            //{
+            //    // TO-DO: Si no es admin, redireccionarlo a otra página que sea un aviso.
+            //    Response.Redirect("PaginaNoAutorizada.aspx");
+            //}
         }
     }
 }
